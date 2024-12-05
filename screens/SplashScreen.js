bec1bbc97e79aafe60b72becb169833620e6c611
riyasati <115941306+riyasati@users.noreply.github.com>
2024-12-05 13:10:11 +0530

    Create SplashScreen.js

---
+++ screens/SplashScreen.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+const SplashScreen = ({ navigation }) => {
+  useEffect(() => {
+    setTimeout(() => navigation.replace('Main'), 2000);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/splash.png')} style={styles.image} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000' },
+  image: { width: 200, height: 200 },
+});
+
+export default SplashScreen;
